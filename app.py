0ae58248bd62c27a371ddf1b79c70176a569b5ab
Leo Anthias <leo@kivo.com>
2015-07-13 12:18:23 +0100

    working service :D:D:D

---
+++ app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Demo StackHut service
+"""
+import stackhut
+from selenium import webdriver
+
+class DemoService:
+    def __init__(self):
+        pass
+
+    def renderWebpage(self, url, width, height):
+        driver = webdriver.PhantomJS(stackhut.root_dir + "/phantomjs-2.0.1-bin") # or add to your PATH
+        driver.set_window_size(width, height) # optional
+        driver.get(url)
+
+        driver.save_screenshot('screen.png') # save a screenshot to disk
+        return stackhut.put_file('screen.png')
+
+# export the services here
+SERVICES = {"Default": DemoService()}
